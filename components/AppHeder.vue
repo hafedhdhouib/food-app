47f3e1ccbe6bac3e3700202a788b11c02be1f85e
hafedhdhouib <34197380+hafedhdhouib@users.noreply.github.com>
2021-03-03 20:19:12 +0100

    Revert "finish"
    This reverts commit 81c0421c5eea4b56a99a078a89b65b81736849f0.

--- components/AppHeder.vue
+++ components/AppHeder.vue
@@ -1,7 +1,8 @@
 <template>
     <section class="masthead" role="img" aria-label="Image Description">
   <h1>
-Hello there  </h1>
+    The Hero Generator
+  </h1>
   <!--  <button>
       When a hero comes along
     </button>-->
